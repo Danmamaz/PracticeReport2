8ed1cda35771b3ec532cbc1e2b7c713ea2bdb131
Danylo <danilolitvin904@gmail.com>
2025-05-16 14:33:14 +0300

    16.05.25

--- gui/game_app.py
+++ gui/game_app.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from models.entity import *
 
 
 class App(ctk.CTk):
@@ -6,9 +7,77 @@ class App(ctk.CTk):
         super().__init__()
         self.geometry("700x400")
         self.title("Game")
+        self.resizable(False, False)
+        self.tabview = None
+        self.start_button = None
+        self.player_buttons = []
+        self.round_counter = 0
         ctk.set_appearance_mode("light")
 
-        # create_ui()
+        self.create_ui()
+
+    def create_ui(self):
+        # Creating main UI component
+        self.tabview = VerticalTabView(self, {"Fight": None, "Store": None, "Tutorial": None})
+        self.tabview.pack(expand=True, fill="both")
+
+        # Buttons for game and tutorial
+        self.start_button = ctk.CTkButton(self.tabview.tabs["Fight"], text="Start new run",
+                                          command=self.init_fight)
+        self.start_button.pack()
+
+    def init_fight(self, player=Warrior(100, 0, (24, 30)),
+                        enemy=Entity(100, 0, (24, 30))):
+
+        # forgetting old button and create point variable for objects to pack
+        self.start_button.pack_forget()
+        land = self.tabview.tabs["Fight"]
+
+        # Enemy UI
+        enemy_name_l = ctk.CTkLabel(land, text="Enemy name")
+        enemy_name_l.pack(padx=10)
+
+        enemy_sprite = ctk.CTkLabel(land, width=75, height=75, fg_color="gray", text="")
+        enemy_sprite.pack()
+
+        enemy_health = ctk.CTkProgressBar(land, mode="determinate")
+        enemy_health.set((enemy.health * enemy.max_health / 100) / 100)
+        enemy_health.pack()
+
+        # Spacer between enemy and player
+        spacer = ctk.CTkLabel(land, height=150, text="")
+        spacer.pack()
+
+        # Player UI
+        player_sprite = ctk.CTkLabel(land, width=75, height=75, fg_color="gray", text="")
+        player_sprite.pack()
+
+        player_health = ctk.CTkProgressBar(land, mode="determinate")
+        player_health.set((player.health * player.max_health / 100) / 100)
+        player_health.pack()
+
+        player_options = ctk.CTkFrame(land)
+        for i, btn in enumerate(["Attack", "Defend", f"{player.unique_option_name}"]):
+            option = ctk.CTkButton(player_options, text=btn, width=80, height=30, state="disabled",
+                                   command=player.options[i] if i != 0 else lambda t=enemy, hpb=enemy_health: player.attack(t, hpb))
+            option.pack(pady=15, side="left")
+            self.player_buttons.append(option)
+
+        player_options.pack()
+
+    def toggle_moves(self):
+        if self.round_counter % 2 == 0:
+            player_move()
+        else:
+            ai_move()
+        self.round_counter += 1
+
+    def player_move(self):
+        for button in self.player_buttons:
+            button.configure(state="normal")
+        self.round_counter += 1
+
+    def ai_move(self):
 
 
 class VerticalTabView(ctk.CTkFrame):
@@ -20,16 +89,17 @@ class VerticalTabView(ctk.CTkFrame):
         self.current_tab = None
         self.current_button = None
 
-        self.left_frame = ctk.CTkFrame(self)
+        self.left_frame = ctk.CTkFrame(self, width=200)
+        self.left_frame.pack_propagate(False)
         self.left_frame.pack(side="left", fill="y")
 
         self.right_frame = ctk.CTkFrame(self)
         self.right_frame.pack(side="left", fill="both", expand=True)
 
         for name in tabs:
-            btn = ctk.CTkButton(self.left_frame, text=name)
+            btn = ctk.CTkButton(self.left_frame, text=name, width=150, height=40)
             btn.configure(command=lambda b=btn, n=name: self.switch_tab(n, b))
-            btn.pack(pady=5, fill="x")
+            btn.pack(pady=10)
 
             frame = ctk.CTkFrame(self.right_frame)
             self.tabs[name] = frame
@@ -47,4 +117,3 @@ class VerticalTabView(ctk.CTkFrame):
             button.configure(fg_color="transparent")
         btn.configure(fg_color="green")
         self.current_button = btn
-
--- models/entity.py
+++ models/entity.py
@@ -1,28 +1,33 @@
+from random import choice
+
+
 class Entity:
     def __init__(self, health: int, armor: int, damage_range: tuple):
         self.health = health
+        self.max_health = health
         self.armor = armor
         self.damage = range(*damage_range)
         self.heal_amount = self.health * 20/100
         self.defend_counter = 0
+        self.options = [self.attack, self.defend, self.unique_option]
 
-    def attack(self, target: Entity):
+    def attack(self, target, target_health_bar):
         target.take_damage(self.damage)
+        target_health_bar.set((target.health * target.max_health / 100) / 100)
         return "Attacked!"
 
     def defend(self):
         self.defend_counter += 1
         return f"You have {self.defend_counter} damage blocks!"
 
-    def heal(self):
-        self.health += self.heal_amount
-        return f"You healed by {self.heal_amount} points"
+    def unique_option(self):
+        pass
 
     def take_damage(self, amount: int):
         if self.defend_counter:
             self.defend_counter -= 1
             return "Damage blocked!"
-        self.health -= amount * self.armor / 100
+        self.health -= amount
         return f"-{amount} health"
 
     def show_info(self):
@@ -35,9 +40,23 @@ class Entity:
 
 
 class Warrior(Entity):
-    def __init__(self, health=120, armor=10, damage_range=range(18, 24)):
+    def __init__(self, health: int, armor: int, damage_range: tuple):
         super().__init__(health, armor, damage_range)
+        self.options = [self.attack, self.defend, self.buff]
+        self.buff_counts = 0
+        self.buff_mult = 2
+        self.unique_option_name = "Buff"
 
-    def scale_damage(self, scale=1.1):
-        self.damage *= scale
+    def buff(self):
+        self.buff_counts += 1
+        return f"Next attack will deal {self.buff_mult}x damage"
 
+    def attack(self, target, target_health_bar):
+        if self.buff_counts != 0:
+            damage_amount = choice(self.damage) * self.buff_mult
+            self.buff_counts -= 1
+        else:
+            damage_amount = choice(self.damage)
+        target.take_damage(damage_amount)
+        target_health_bar.set((target.health * target.max_health / 100) / 100)
+        return "Attacked!"
